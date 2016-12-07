3e753a354d67cb691290a1eb17c8fba7902b16fa
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-12-07 11:28:33 -0600

    Put Node and LinkedLists into seperate correctly named spec files

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -1,25 +1,11 @@
 require 'rspec'
-require_relative 'node'
 
-describe 'node' do
-  let(:first_node) { Node.new('first') }
-  let(:node_with_link) { first_node.insert_after(Node.new('second')) }
+require_relative 'linked_list'
+
+describe 'LinkedList' do
   context '#new' do
-    it 'contains correct element' do
-      expect(first_node.element).to eq 'first'
-    end
-  end
-  context '#insert_after' do
-    it 'links parent node to a child' do
-      child_node = Node.new('child')
-      first_node.insert_after(child_node)
-      expect(first_node.linked_node).to be child_node
-    end
-  end
-  context '#remove_after' do
-    it 'removes child node' do
-      node_with_link.remove_after()
-      expect(node_with_link.linked_node).to be nil 
+    it 'instantiates a new linked list' do
+      expect(LinkedList.new).to be_kind_of(LinkedList)
     end
   end
 end
