b3ee3e2bb6eaa8d52164825afabb2032e351659a
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-12-07 11:45:39 -0600

    Add first node to a list

--- linked_list.rb
+++ linked_list.rb
@@ -1,2 +1,11 @@
 class LinkedList
+  attr_reader :linked_node
+
+  def initialize
+    @linked_node = nil
+  end
+
+  def insert_first(node)
+    @linked_node = node
+  end
 end
--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -1,11 +1,20 @@
 require 'rspec'
 
 require_relative 'linked_list'
+require_relative 'node'
 
 describe 'LinkedList' do
+  let (:list) { LinkedList.new }
   context '#new' do
     it 'instantiates a new linked list' do
       expect(LinkedList.new).to be_kind_of(LinkedList)
+    end
+  end
+  context '#insert_first' do
+    it 'inserts element at front of list' do
+      node = Node.new('first')
+      list.insert_first(node)
+      expect(list.linked_node).to match(node)
     end
   end
 end
