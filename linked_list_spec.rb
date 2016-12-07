43dbeb1298a1e01ab17be243c38f5e6d1e3abbab
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-12-07 10:38:57 -0600

    DRY up node tests using let block to instantiate first node for each

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -2,18 +2,17 @@ require 'rspec'
 require_relative 'node'
 
 describe 'node' do
+  let(:first_node) { Node.new('first') }
   context '#new' do
     it 'contains correct element' do
-      node = Node.new('thingy')
-      expect(node.element).to eq 'thingy'
+      expect(first_node.element).to eq 'first'
     end
   end
   context '#insert_after' do
     it 'links parent node to a child' do
-      parent_node = Node.new('parent')
       child_node = Node.new('child')
-      parent_node.insert_after(child_node)
-      expect(parent_node.linked_node).to be child_node
+      first_node.insert_after(child_node)
+      expect(first_node.linked_node).to be child_node
     end
   end
 end
