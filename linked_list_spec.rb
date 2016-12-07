f58f1fa9b3d51cefd298fc96e4557093a398338b
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-12-07 10:03:37 -0600

    Reword node initialization test

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -3,7 +3,7 @@ require_relative 'node'
 
 describe 'node' do
   context '#new' do
-    it 'should have node containing correct element' do
+    it 'contains correct element' do
       node = Node.new('thingy')
       expect(node.element).to eq 'thingy'
     end
